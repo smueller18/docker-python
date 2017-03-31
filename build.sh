e9cec33c00f1f50c42c474cac00fa5f7933b514b
Stephan Müller <mail@stephanmueller.eu>
2017-03-31 08:47:04 +0200

    change container names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t "smueller18/python2-alpine" ./python2-alpine
-docker build -t "smueller18/python3-alpine" ./python3-alpine
+docker build -t "smueller18/python:2-alpine" ./python2-alpine
+docker build -t "smueller18/python:3-alpine" ./python3-alpine
