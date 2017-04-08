a5da28c49a83d45c410895b07c445bc427828554
Stephan Müller <mail@stephanmueller.eu>
2017-04-08 11:02:59 +0200

    add support for adding custom time zone

--- python2-alpine/entrypoint.sh
+++ python2-alpine/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-# adopted from https://github.com/biggis-project/biggis-infrastructure/blob/master/biggis-docker/biggis-pipeline/biggis-base/alpine/entrypoint.sh
-
 # Add local user
 # Either use the USER_ID if passed in at runtime or
 # fallback
 
 USER_ID=${USER_ID:-9001}
 USER=${USER_NAME:-user}
+
+# Set the timezone. Base image does not contain the setup-timezone script, so an alternate way is used.
+if [ -n "$TIMEZONE" ]; then
+    cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime && \
+	echo "${TIMEZONE}" >  /etc/timezone && \
+	echo "Container timezone set to: $TIMEZONE"
+else
+	echo "Container timezone not modified"
+fi
 
 echo "Starting with UID : $USER_ID"
 addgroup -S -g $USER_ID $USER
